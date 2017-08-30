30efff62878e9089f112ba415befdcff68737c37
wangwei <244919681@qq.com>
2017-08-30 22:23:27 +0800

    first Commit

---
+++ firstSpringBoot/src/main/java/MasterSpringMvcApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringBootConfiguration;
+
+/**
+ * Created by admin on 2017/8/29.
+ */
+@SpringBootConfiguration
+public class MasterSpringMvcApplication {
+
+
+
+}
